657bd197dbea72d87dccba4ae859d1e0c9515f11
Daniel Henriquez <contacto@dhenriquez.cl>
2018-01-24 15:03:31 -0300

    se crea funcion registrar

--- codeigniter/application/models/usuario_model.php
+++ codeigniter/application/models/usuario_model.php
@@ -2,8 +2,27 @@
 
 class Usuario_model extends CI_Model{
 	
-	public function getusuarios(){
-		return 'si';
+	public $nombre;
+	public $email;
+	public $usuario;
+	public $password;
+	public $activo = 0;
+		
+	public function get_usuarios(){
+		$query = $this->db->get('usuario');
+        return $query->result();
+	}
+	
+	public function registrar($datos = array()){
+		$this->usuario_nombre = $datos['nombre'];
+		$this->usuario_email = $datos['email'];
+		$this->usuario = $datos['usuario'];
+		$this->password = $datos['password'];
+		
+		$this->db->insert('usuario',$this);
+		
+		return ($this->db->affected_rows() != 1) ? false : true;
+		
 	}
 	
 }
