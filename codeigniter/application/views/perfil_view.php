0dd751fe86a894879dd0a47c7b92ee66efddcbc5
Daniel Henriquez <contacto@dhenriquez.cl>
2018-01-24 16:44:30 -0300

    se crea pagina de perfil

---
+++ codeigniter/application/views/perfil_view.php
@@ -0,0 +1,23 @@
+<main role="main">
+	<div class="perfil">
+		<div class="container">
+			<div class="titulo row">
+				<div class="col-12 text-center">
+					<h1 class="display-1">
+						<?php echo $titulo; ?>
+					</h1>
+					<h4 class="display-4">
+						<?php echo $descripcion; ?>
+					</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 ">
+				perfil
+			</div>
+		</div>
+	</div>
+</main><!-- /.container -->
