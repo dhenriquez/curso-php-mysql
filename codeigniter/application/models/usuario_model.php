9f12663f3966c9c3f1a22889bab7e16778572f91
Daniel Henriquez <contacto@dhenriquez.cl>
2018-01-24 16:43:45 -0300

    login de usuario ok

--- codeigniter/application/models/usuario_model.php
+++ codeigniter/application/models/usuario_model.php
@@ -8,9 +8,11 @@ class Usuario_model extends CI_Model{
 	public $password;
 	public $activo = 0;
 		
-	public function get_usuarios(){
-		$query = $this->db->get('usuario');
-		return $query->result();
+	public function login($datos = array()){
+		$this->usuario = $datos['usuario'];
+		$this->password = $datos['password'];
+		$query = $this->db->get_where('usuario',array('usuario'=>$this->usuario,'password'=>$this->password));
+		return ($query->num_rows()>0) ? $query->row() : false;
 	}
 	
 	public function registrar($datos = array()){
@@ -18,16 +20,14 @@ class Usuario_model extends CI_Model{
 		$this->usuario_email = $datos['email'];
 		$this->usuario = $datos['usuario'];
 		$this->password = $datos['password'];
-		
 		$this->db->insert('usuario',$this);
-		
 		return ($this->db->affected_rows() != 1) ? false : $this->db->insert_id();
 		
 	}
 	
 	public function validar($key){
 		$this->db->set('activo',1);
-		$this->db->where('md5(id)',$key);
+		$this->db->where('md5(usuario_ID)',$key);
 		$this->db->update('usuario');
 		return ($this->db->affected_rows() == 1) ? true : false;
 	}
