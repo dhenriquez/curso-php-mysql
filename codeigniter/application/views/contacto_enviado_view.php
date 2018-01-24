f48de0b323666f048a513f120754a0ce91d6980b
Daniel Henriquez <contacto@dhenriquez.cl>
2018-01-24 13:03:25 -0300

    cambiar tamaño texto de confirmación

--- codeigniter/application/views/contacto_enviado_view.php
+++ codeigniter/application/views/contacto_enviado_view.php
@@ -15,8 +15,8 @@
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-12 ">
-				<h1 class="display-1">
+			<div class="col-12 text-center">
+				<h1>
 					<?php echo $enviado; ?>
 				</h1>
 				<p><?php echo anchor('sitio/contacto', 'Volver'); ?></p>
