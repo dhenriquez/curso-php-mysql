988e4630d0813803b58832506726bd44d164b780
Daniel Henriquez <contacto@dhenriquez.cl>
2018-01-24 10:27:31 -0300

    formato de codigo

--- codeigniter/application/views/index_view.php
+++ codeigniter/application/views/index_view.php
@@ -3,8 +3,12 @@
 		<div class="container">
 			<div class="titulo row">
 				<div class="col-12 text-center">
-					<h1 class="display-1"><?php echo $titulo; ?></h1>
-					<h4 class="display-4"><?php echo $descripcion; ?></h4>
+					<h1 class="display-1">
+						<?php echo $titulo; ?>
+					</h1>
+					<h4 class="display-4">
+						<?php echo $descripcion; ?>
+					</h4>
 				</div>
 			</div>
 		</div>
--- codeigniter/application/views/youtube_view.php
+++ codeigniter/application/views/youtube_view.php
@@ -3,8 +3,12 @@
 		<div class="container">
 			<div class="titulo row">
 				<div class="col-12 text-center">
-					<h1 class="display-1"><?php echo $titulo; ?></h1>
-					<h4 class="display-4"><?php echo $descripcion; ?></h4>
+					<h1 class="display-1">
+						<?php echo $titulo; ?>
+					</h1>
+					<h4 class="display-4">
+						<?php echo $descripcion; ?>
+					</h4>
 				</div>
 			</div>
 		</div>
@@ -13,16 +17,19 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="card-group">
-				<?php
-				for($i=0; $i<5; $i++){ ?>
-						<div class="card">
-							<img class="card-img-top" src="<?php echo $youtube[$i]['imagen'];?>" alt="<?php echo $youtube[$i]['titulo'];?>">
-							<div class="card-body">
-								<h5 class="card-title"><?php echo ($i+1) . ". " . $youtube[$i]['titulo'];?></h5>
-								<a href="<?php echo $youtube[$i]['link'];?>" class="btn btn-primary" target="_blank">Ver video</a>
-							</div>
+					<?php
+					for ( $i = 0; $i < 5; $i++ ) {
+						?>
+					<div class="card">
+						<img class="card-img-top" src="<?php echo $youtube[$i]['imagen'];?>" alt="<?php echo $youtube[$i]['titulo'];?>">
+						<div class="card-body">
+							<h5 class="card-title">
+								<?php echo ($i+1) . ". " . $youtube[$i]['titulo'];?>
+							</h5>
+							<a href="<?php echo $youtube[$i]['link'];?>" class="btn btn-primary" target="_blank">Ver video</a>
 						</div>
-				<?php }; ?>
+					</div>
+					<?php }; ?>
 				</div>
 			</div>
 		</div>
@@ -30,16 +37,19 @@
 		<div class="row">
 			<div class="col-12">
 				<div class="card-group">
-				<?php
-				for($i=5; $i<10; $i++){ ?>
-						<div class="card">
-							<img class="card-img-top" src="<?php echo $youtube[$i]['imagen'];?>" alt="<?php echo $youtube[$i]['titulo'];?>">
-							<div class="card-body">
-								<h5 class="card-title"><?php echo ($i+1) . ". " . $youtube[$i]['titulo'];?></h5>
-								<a href="<?php echo $youtube[$i]['link'];?>" class="btn btn-primary" target="_blank">Ver video</a>
-							</div>
+					<?php
+					for ( $i = 5; $i < 10; $i++ ) {
+						?>
+					<div class="card">
+						<img class="card-img-top" src="<?php echo $youtube[$i]['imagen'];?>" alt="<?php echo $youtube[$i]['titulo'];?>">
+						<div class="card-body">
+							<h5 class="card-title">
+								<?php echo ($i+1) . ". " . $youtube[$i]['titulo'];?>
+							</h5>
+							<a href="<?php echo $youtube[$i]['link'];?>" class="btn btn-primary" target="_blank">Ver video</a>
 						</div>
-				<?php }; ?>
+					</div>
+					<?php }; ?>
 				</div>
 			</div>
 		</div>
