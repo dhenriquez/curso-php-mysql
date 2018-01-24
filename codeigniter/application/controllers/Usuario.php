b370e8ac4a405e151afc7f19030b349bbd9b1990
Daniel Henriquez <contacto@dhenriquez.cl>
2018-01-24 15:20:57 -0300

    cambios en el registro de usuario

--- codeigniter/application/controllers/Usuario.php
+++ codeigniter/application/controllers/Usuario.php
@@ -31,42 +31,55 @@ class Usuario extends CI_Controller {
 	
 	public function registrar(){
 		
-		$this->load->model('usuario_model');
-		$res = $this->usuario_model->getusuarios();
-		
 		$header = array(
 			'active' => __FUNCTION__
 		);
-		
 		$data = array(
 			'titulo' => '¡Registrar!',
 			'descripcion' => 'Aquí puedes completar tus datos para poder registrarte.',
-			'enviado' => $this->input->post('nombre') . ' revisa tu correo para validar tu registro!',
-			'query' => ''
 		);
 		
 		$this->form_validation->set_rules('nombre', 'Nombre completo', 'required', array('required' => 'El %s es requerido.'));
-		$this->form_validation->set_rules('email', 'Email', 'required', array('required' => 'El %s es requerido.'));
-		$this->form_validation->set_rules('usuario', 'Usuario', 'required', array('required' => 'El %s es requerido.'));
+		$this->form_validation->set_rules('email', 'Email', 'required|is_unique[usuario.usuario]', array('required' => 'El %s es requerido.','is_unique'=>'El %s ya se encuentra en uso.'));
+		$this->form_validation->set_rules('usuario', 'Usuario', 'required|is_unique[usuario.usuario]', array('required' => 'El %s es requerido.','is_unique'=>'El %s ya se encuentra en uso.'));
 		$this->form_validation->set_rules('password', 'Contraseña', 'required', array('required' => 'El %s es requerida.'));
 		
 		$this->load->view('header', $header);
 		if($this->form_validation->run() == FALSE){
 			$this->load->view(__FUNCTION__.'_view', $data);
 		}else{
-			/*$email = $this->input->post('email');
+			$email = $this->input->post('email');
 			$nombre = $this->input->post('nombre');
 			$usuario = $this->input->post('usuario');
 			$password = $this->input->post('password');
-			$asunto = 'Registro de usuario';
-			$mensaje = '';
 			
-			$this->email->from($email,$nombre);
-			$this->email->to($this->email_para);
-			$this->email->subject($asunto);
-			$this->email->message($mensaje);
-			$this->email->send();*/
-			$this->load->view(__FUNCTION__.'_enviado_view', $data);
+			$registro['email'] = $email;
+			$registro['nombre'] = $nombre;
+			$registro['usuario'] = $usuario;
+			$registro['password'] = $password;
+			
+			$res = $this->usuario_model->registrar($registro);
+			
+			if($res){
+				
+				$data = array(
+					'titulo' => '¡Gracias!',
+					'descripcion' => 'Ya te encuentras inscrito en nuestro sitio!',
+					'enviado' => $nombre . ' revisa tu correo para validar tu registro!',
+				);
+				$asunto = 'Registro de usuario';
+				$mensaje = 'Te acabas de registrar en nuestro sitio.';
+
+				$this->email->from('no-responder@dhenriquez.cl','Sitio de Prueba');
+				$this->email->to($email);
+				$this->email->subject($asunto);
+				$this->email->message($mensaje);
+				$this->email->send();
+				$this->load->view(__FUNCTION__.'_enviado_view', $data);
+				
+			}else{
+				$this->load->view(__FUNCTION__.'_view', $data);
+			}
 		}
 		$this->load->view('footer');
 	}
