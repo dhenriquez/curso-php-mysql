cc0699b2e6e8d8178bb9cf781ed2163669ae9819
Daniel Henriquez <contacto@dhenriquez.cl>
2018-01-24 13:04:11 -0300

    pagina de confirmación de registro

---
+++ codeigniter/application/views/registrar_enviado_view.php
@@ -0,0 +1,26 @@
+<main role="main">
+	<div class="contacto">
+		<div class="container">
+			<div class="titulo row">
+				<div class="col-12 text-center">
+					<h1 class="display-1">
+						<?php echo $titulo; ?>
+					</h1>
+					<h4 class="display-4">
+						<?php echo $descripcion; ?>
+					</h4>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12 text-center">
+				<h1>
+					<?php echo $enviado; ?>
+				</h1>
+				<p><?php echo anchor('usuario/login', 'Volver'); ?></p>
+			</div>
+		</div>
+	</div>
+</main><!-- /.container -->
